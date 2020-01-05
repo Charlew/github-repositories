2a3f80bec17054b2c5d1de40666c98ed1df00f63
Charlew <kwisniewski103@gmail.com>
2020-01-05 20:48:05 +0100

    Remove GithubRepositoryFacade interface

--- src/main/java/com/recruit/githubrepositories/domain/GithubRepositoryFacade.java
+++ src/main/java/com/recruit/githubrepositories/domain/GithubRepositoryFacade.java
@@ -1,6 +1,18 @@
 package com.recruit.githubrepositories.domain;
 
-public interface GithubRepositoryFacade {
+import com.recruit.githubrepositories.infrastructure.dao.GithubRepositoryRestApiClient;
+import org.springframework.stereotype.Service;
 
-    GithubRepository get(String owner, String repositoryName);
+@Service
+public final class GithubRepositoryFacade {
+
+    private final GithubRepositoryRestApiClient githubRepositoryClient;
+
+    public GithubRepositoryFacade(GithubRepositoryRestApiClient githubRepositoryClient) {
+        this.githubRepositoryClient = githubRepositoryClient;
+    }
+
+    public GithubRepository get(String owner, String repositoryName) {
+        return githubRepositoryClient.getRepositoryData(owner, repositoryName);
+    }
 }
